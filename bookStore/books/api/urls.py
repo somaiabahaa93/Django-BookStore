0c9fb77cd2a41160765036b63df29b9148af6a7e
somaiabahaa93 <somaianaggar93@gmail.com>
2021-04-25 01:50:47 +0200

    final task

--- bookStore/books/api/urls.py
+++ bookStore/books/api/urls.py
@@ -5,7 +5,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('login',obtain_auth_token),
     path('signup',views.api_signup),
-     path('',views.index),
-    path('create',views.create)
+    path('',views.index),
+    path('create',views.create),
+    path('edit',views.edit),
+    path("delete/<int:id>",views.delete),
+    path("edit/<int:id>",views.edit),
+    path("show/<int:id>",views.show),
+
+
 
 ]
--- bookStore/books/api/views.py
+++ bookStore/books/api/views.py
@@ -4,6 +4,7 @@ from books.models import Book
 from .serializers import BookSerializer,UserSerializer
 from rest_framework.permissions import IsAuthenticated,BasePermission
 from rest_framework.decorators import api_view,permission_classes
+# from rest_framework.parsers import JSONParser
 
 @api_view(["POST"])
 def api_signup(request):
@@ -30,7 +31,7 @@ def api_signup(request):
 
 
 @api_view(["GET"])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def index(request):
     books=Book.objects.all()
     serializer=BookSerializer(books,many=True)
@@ -49,3 +50,57 @@ def create(request):
         "success":False,
          "errors":serializer.errors 
      },status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+def edit(request, id):
+    book=Book.objects.get(pk=id)
+    serializer=BookSerializer(book, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(data={
+            "success":True,
+            "message":"book has been updated successfuly"
+        },status=status.HTTP_201_CREATED)
+
+    return Response(data={
+            "success":False,
+            "errors":serializer.errors
+    },status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def show(request,id):
+    book=Book.objects.get(pk=id)
+    serializer=BookSerializer(instance=book)
+    return Response(data=serializer.data,status=status.HTTP_200_OK)   
+
+    
+
+
+@api_view(["DELETE"])
+def delete(request, id):
+    try: 
+
+        book=Book.objects.get(pk=id)
+
+        book.delete()
+
+        return Response(data={
+
+        "success":True,
+
+        "message":"Book has been deleted successfully",
+
+        },
+
+        status=status.HTTP_200_OK,
+
+        )
+
+    except Book.DoesNotExist: 
+
+        return JsonResponse({'message': 'This book does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+
+
